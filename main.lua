e9d25dbdb1b4f954d66c6ceb566faa8b1620f24a
Jeremy Runyan <GoogleBot42@users.noreply.github.com>
2015-05-02 16:14:32 -0600

    Made the demo better

--- main.lua
+++ main.lua
@@ -21,15 +21,14 @@
 -- SOFTWARE.
 
 local CoYield = require "libCoYield"
-
-local env = {print = print, j = 0}
+local env = require("SandboxEnv")()
+env.j = 0
 local function CoFunc()
-	print("h") 
-	local i
-	for i=1,2000 do 
-		j=j+1 -- keeps luaJIT from optimizing this loop out of existence
+	print("It starts...") 
+	while true do
+		-- never ends... or does it?
 	end
-	print("/H")
+	print("It finishes.")
 end
 local function safeCall()
 	local success, msg = pcall(CoFunc)
@@ -44,11 +43,9 @@ setfenv(CoFunc,env)
 local co = coroutine.create(safeCall)
 CoYield.MakeCoYield(co)
 
-local stillRunning = true
-while stillRunning do
-	-- must reenable yielding before each resume because otherwise the C function 
-	-- does weird things and tries to yield outside this coroutine for some strange reason
-	CoYield.ReenableYield()
-	stillRunning = coroutine.resume(co)
-	print(stillRunning, env.j, coroutine.status(co))
-end
+-- must reenable yielding before each resume because otherwise the C function 
+-- does weird things and tries to yield outside this coroutine for some strange reason
+CoYield.ReenableYield()
+stillRunning = coroutine.resume(co)
+
+print("But it doesn't finish!")
